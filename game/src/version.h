ecc87539aae98e356c893076ca35ff88a335db43
Daivuk <daivuk@gmail.com>
2023-11-21 19:34:27 -0500

    increment version

--- game/src/version.h
+++ game/src/version.h
@@ -3,7 +3,7 @@
 
 #define DAX_MAJOR 0
 #define DAX_MINOR 2
-#define DAX_PATCH 0
+#define DAX_PATCH 1
 #define DAX_STR(x) DAX_STR2(x)
 #define DAX_STR2(x) #x
 #define DAX_VERSION DAX_STR(DAX_MAJOR) "." DAX_STR(DAX_MINOR) "." DAX_STR(DAX_PATCH)
